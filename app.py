56ee58a6fa473cefb5a7a973ac43a6782ecc2288
abdallah <aharundd@gmail.com>
2024-06-26 13:37:33 +0100

    fixing test fail again

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_home():
-    return "Hello, world!" # changed to make test fail then changed back again
+    return "Hello, world!"
 
 if __name__ == '__main__':
     app.run(

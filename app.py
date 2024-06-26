e889b91c8c6140de421c8aab2a2bb1ea884cdc48
abdallah <aharundd@gmail.com>
2024-06-26 13:31:10 +0100

    test fail test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_home():
-    return "Hello, world!"
+    return "Hello, wor" # changed to make test fail
 
 if __name__ == '__main__':
     app.run(
